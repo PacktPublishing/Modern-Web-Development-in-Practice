c6d44a7cba2e9897bc00661daed78d63022b0642
Benjamin Howarth <me@benjaminhowarth.ltd>
2019-05-09 13:13:49 +0100

    Section 4.3 added Gatsby static data to GraphQL

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,6 +1,26 @@
+const menuLinks = [
+    { url: '/', name: 'Home' },
+    { url: '/contact/', name: 'Contact' },
+    { url: '/about/', name: 'About' }
+  ]
+
+const siteData = {
+    siteMetadata: {
+        title: 'Gatsby Starter Blog',
+        author: 'Kyle Mathews',
+        description: 'A starter blog demonstrating what Gatsby can do.',
+        siteUrl: 'https://gatsby-starter-blog-demo.netlify.com/',
+        social: {
+          twitter: 'kylemathews',
+        },
+        menuLinks: () => menuLinks
+    }
+}
+
 const resolvers = {
     Query: {
-        hello: () => "world!"
+        hello: () => "world!",
+        site: () => siteData
     }
 }
 
--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -2,7 +2,30 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
     type Query {
         "A simple test property to hel you get started with GraphQL"
-        hello: String
+        hello: String,
+        site: Site
+    }
+
+    type Site {
+        siteMetadata: SiteMetadata
+    }
+
+    type SiteMetadata {
+        title: String,
+        author: String,
+        description: String,
+        siteUrl: String,
+        social: SocialMetadata,
+        menuLinks: [MenuLink]
+    }
+
+    type SocialMetadata {
+        twitter: String
+    }
+
+    type MenuLink {
+        name: String,
+        url: String
     }
 `
 
